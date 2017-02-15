1836cbf8abc17ca1dcb934833932866322b3e0e8
Hiram <hiram0101@gmail.com>
2017-02-14 23:25:57 -0500

    AI Changes

--- lib/players/players.rb
+++ lib/players/players.rb
@@ -1,5 +1,7 @@
 module Players
 
+  WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
   class Human < Player
     def move(board = [])
       input = gets
@@ -9,97 +11,88 @@ module Players
 
   class Computer < Player
 
-    attr_accessor :options, :root_node
-
-    WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
-    def initialize(token)
+    def initialize(token = "X")
       super
-      @root_node = BoardNode.new(Array.new(9), @token)
-      GameTree.new.generate(@root_node)
+      @game_state = GameState.new(Array.new(9))
+      GameTree.new.generate(@game_state, "X", @token)
     end
 
     def move(board)
-      if @token.eql?("X") && @root_node.board.compact.size.eql?(0)
-        index = Random.new.rand(8)
-        @root_node = @root_node.children[index]
-        return (@root_node.last_move[:index] + 1).to_s
-      else
-        new_board = board.map{|cell| cell.eql?(" ") ? nil : cell}
-        @root_node.children.each do |child|
-          if child.board.eql?(new_board)
-            @root_node = child
-          end
+      check_board = board.dup
+      check_board.map!{|e| e.eql?(" ") ? nil : e}
+
+      next_game_state = @game_state.children.detect{|e| child.board.eql?(check_board)}
+      @game_state = next_game_state.children.max{|a, b| a.rank <=> b.rank}
+
+      (@game_state.last_move[:index] + 1).to_s
+    end
+
+  end
+
+  class GameState
+    attr_accessor :children, :board, :last_move, :own_token
+
+    def initialize(board)
+      @board = board
+      @children = []
+    end
+
+    def is_over?
+      is_won? || is_over?
+    end
+
+    def is_won?
+      WIN_COMBOS.any? do |combo|
+        @board[combo[0]].eql?(@board[combo[1]]) && @board[combo[1]].eql?(@board[combo[2]]) && !@board[combo[0]].nil?
+      end
+    end
+
+    def is_draw?
+      !is_won? && @board.compact.size.eql?(9)
+    end
+
+    def winner
+      WIN_COMBOS.each do |combo|
+        if @board[combo[0]].eql?(@board[combo[1]]) && @board[combo[1]].eql?(@board[combo[2]]) && !@board[combo[0]].nil?
+          return @board[combo[0]]
         end
-        @root_node.children.each{|child| print "#{child.rank} "}
-        @root_node = @root_node.children.max{ |a, b| a.rank <=> b.rank }
-        return (@root_node.last_move[:index] + 1).to_s
       end
     end
 
-  end
-
-end
-
-class BoardNode
-
-  attr_accessor :board, :children, :last_move, :own_token, :rank, :winner
-
-  WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
-  def initialize(board = Array.new(9), own_token = "X", last_move = {:index => nil, :token => nil})
-    @board = board
-    @children = []
-    @own_token = own_token
-    @last_move = last_move
-  end
-
-  def rank
-    @rank ||= final_state_rank || intermediate_state_rank
-  end
-
-  def final_state_rank
-    if is_won? || is_draw?
-      return 0 if is_draw?
-      @winner.eql?(@own_token) ? 10 - @children.size : @children.size - 10
+    def rank
+      @rank ||= final_state_rank || intermediate_state_rank
     end
-  end
 
-  def intermediate_state_rank
-    ranks = @children.collect{ |child| child.rank }
-    if @last_move[:token] == @own_token
-      ranks.max
-    else
-      ranks.min
+    def final_state_rank
+      if is_over?
+        return 0 if is_draw?
+        winner.eql?(@own_token) ? 10 : -10
+      end
     end
-  end
 
-  def is_won?
-    WIN_COMBOS.any? do |combo|
-      if (@board[combo[0]].eql?(@board[combo[1]]) && @board[combo[1]].eql?(@board[combo[2]]) && !@board[combo[0]].nil?)
-        @winner = @board[combo[0]]
-        true
+    def intermediate_state_rank
+      ranks = @children.collect{ |game_state| game_state.rank }
+      if @last_move[:token].eql?(@own_token)
+        ranks.max
       else
-        false
+        ranks.min
       end
     end
-  end
 
-  def is_draw?
-    !is_won? && @board.compact.size.eql?(9)
-  end
-end
+  class GameTree
+    def generate(current_game_state, current_player, own_token)
+      next_player = current_player.eq?("X") ? "O" : "X"
+      current_game_state.board.each_with_index do |x, y|
+        if x.nil?
+          next_board = current_game_state.board.dup
+          next_board[y] = current_player
+          next_game_state = GameState(next_board)
+          next_game_state.last_move = {:index => y, :token => current_player}
+          next_game_state.own_token = own_token
 
-class GameTree
-  def generate(current_node, current_player = "X")
-    next_player = current_player.eql?("X") ? "O" : "X"
-    current_node.board.each_with_index do |cell, index|
-      if cell.nil?
-        next_node = BoardNode.new(current_node.board.dup, current_node.own_token, { :index => index, :token => current_player })
-        next_node.board[index] = current_player
-        current_node.children << next_node
-
-        generate(next_node, next_player)
+          current_game_state.children << next_game_state
+          generate(next_game_state, next_player, own_token)
+        end
       end
     end
   end
