c281cf002d37e78523752b396a760594606e51ae
Ahmad Abdel-Halim <ahmadabdelhalimmoustafa@gmail.com>
2017-03-13 16:17:54 +0000

    finished the game

--- lib/board.rb
+++ lib/board.rb
@@ -1,10 +1,7 @@
 class Board
-
   attr_accessor :cells
 
-  WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
-  def initialize
+  def initialize()
     reset!
   end
 
@@ -13,43 +10,34 @@ class Board
   end
 
   def display
-    @cells.each_with_index do |cell, i|
-      if i % 3 == 0
-        puts " #{@cells[i]} | #{@cells[i+1]} | #{@cells[i+2]} "
-        if !i.eql?(6)
-          puts "-----------"
-        end
-      end
-    end
-  end
-
-  def position(input)
-    @cells[input.to_i - 1]
+    puts " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+    puts "-----------"
+    puts " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+    puts "-----------"
+    puts " #{cells[6]} | #{cells[7]} | #{cells[8]} "
   end
 
   def full?
-    turn_count.eql?(9)
+    cells.all?{|token| token == "X" || token == "O"}
   end
 
   def turn_count
-    @cells.map{ |cell| !cell.eql?(" ") ? cell : nil }.compact.size
-  end
-
-  def taken?(input)
-    !position(input).eql?(" ")
+    cells.count{|token| token == "X" || token == "O"}
   end
 
   def valid_move?(input)
-    (input.to_i - 1 < 9) && (input.to_i - 1 >= 0) && !taken?(input)
+    input.to_i.between?(1,9) && !taken?(input)
   end
 
   def update(input, player)
-    if valid_move?(input)
-      @cells[input.to_i - 1] = player.token
-      true
-    else
-      puts "#{input} is invalid"
-      false
-    end
+    cells[input.to_i-1] = player.token
+  end
+
+  def position(input)
+    cells[input.to_i-1]
+  end
+
+  def taken?(input)
+    !(position(input) == " " || position(input) == "")
   end
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,100 +1,42 @@
 module Players
-
-  WIN_COMBOS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
-
-  class Human < Player
-    def move(board = [])
-      input = gets
-      input.chomp
-    end
-  end
-
   class Computer < Player
-
-    def initialize(token = "X")
-      super
-      @game_state = GameState.new(Array.new(9))
-      GameTree.new.generate(@game_state, "X", @token)
-    end
-
     def move(board)
-      new_board = board.is_a?(Board) ? board.cells.dup : board.dup
-      if new_board.map{|e| e.eql?(" ") ? nil : e}.compact.size > 0
-        check_board = new_board.map{|e| e.eql?(" ") ? nil : e}
-        @game_state = @game_state.children.detect{|e| e.board.eql?(check_board)}
-      end
-      @game_state = @game_state.children.max{|a, b| a.rank <=> b.rank}
-      (@game_state.last_move[:index] + 1).to_s
-    end
+      move = nil
 
-  end
+      # When going first, take the middle square. When going second, take the middle square if it isn't yet taken.
+      if !board.taken?(5)
+        move = "5"
 
-  class GameState
-    attr_accessor :children, :board, :last_move, :own_token
+      # If going second and the middle square is taken, take the upper-left corner square.
+      elsif board.turn_count == 1
+        move = "1"
 
-    def initialize(board)
-      @board = board
-      @children = []
-    end
+      # If you went first (and took the middle), take a corner square with your second move.
+      elsif board.turn_count == 2
+        move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
 
-    def is_over?
-      is_won? || is_draw?
-    end
+      # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
+      elsif board.turn_count == 3 && (board.position(1) == board.position(9) || board.position(3) == board.position(7))
+        move = "2"
 
-    def is_won?
-      WIN_COMBOS.any? do |combo|
-        @board[combo[0]].eql?(@board[combo[1]]) && @board[combo[1]].eql?(@board[combo[2]]) && !@board[combo[0]].nil?
-      end
-    end
-
-    def is_draw?
-      !is_won? && @board.compact.size.eql?(9)
-    end
-
-    def winner
-      WIN_COMBOS.each do |combo|
-        if @board[combo[0]].eql?(@board[combo[1]]) && @board[combo[1]].eql?(@board[combo[2]]) && !@board[combo[0]].nil?
-          return @board[combo[0]]
-        end
-      end
-    end
-
-    def rank
-      @rank ||= final_state_rank || intermediate_state_rank
-    end
-
-    def final_state_rank
-      if is_over?
-        return 0 if is_draw?
-        winner.eql?(@own_token) ? 10 : -10
-      end
-    end
-
-    def intermediate_state_rank
-      ranks = @children.collect{ |game_state| game_state.rank }
-      if !@last_move[:token].eql?(@own_token)
-        ranks.max
+      # From here on, run through the WIN_COMBINATIONS array, checking whether any of the combinations have two squares filled with the same token and a third, empty square.
       else
-        ranks.min
-      end
-    end
-  end
+        Game::WIN_COMBINATIONS.detect do |cmb|
 
-  class GameTree
-    def generate(current_game_state, current_player, own_token)
-      next_player = current_player.eql?("X") ? "O" : "X"
-      current_game_state.board.each_with_index do |x, y|
-        if x.nil?
-          next_board = current_game_state.board.dup
-          next_board[y] = current_player
-          next_game_state = GameState.new(next_board)
-          next_game_state.last_move = {:index => y, :token => current_player}
-          next_game_state.own_token = own_token
+          # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
+          if cmb.select{|i| board.position(i+1) == token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
 
-          current_game_state.children << next_game_state
-          generate(next_game_state, next_player, own_token)
+          # If you can't play any winning moves, play a move to block the opponent from winning.
+          elsif cmb.select{|i| board.position(i+1) != " " && board.position(i+1) != token}.size == 2 && cmb.any?{|i| board.position(i+1) == " "}
+            move = cmb.select{|i| !board.taken?(i+1)}.first.to_i.+(1).to_s
+          end
         end
+
+        # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
+        move = [1, 3, 7, 9, 2, 4, 6, 8].detect{|i| !board.taken?(i)}.to_s if move == nil
       end
+      move
     end
   end
 end
