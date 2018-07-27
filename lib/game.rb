65540ee313d2fc841a36333ef899f816165f05bf
Quinn Dizon <qdizon@gmail.com>
2018-07-27 00:26:39 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,7 @@
 
 
 class Game
-  attr_accessor :board, :player_1, :player_2
+  attr_accessor :board, :player_1, :player_2, :cheating_computer
   WIN_COMBINATIONS = [
     [0,1,2],
     [3,4,5],
@@ -17,10 +17,20 @@ class Game
     @player_1 = player_1
     @player_2 = player_2
     @board = board
+    @cheating_computer = false
   end
 
   def current_player
-    self.board.turn_count.even? ? self.player_1 : self.player_2
+    puts "Turn Count: #{self.board.turn_count}"
+    if !@cheating_computer
+      self.board.turn_count.even? ? self.player_1 : self.player_2
+    else
+      if [1,2,4,5,7,8].include?(self.board.turn_count)
+        self.player_2
+      else
+        self.player_1
+      end
+    end
   end
 
   def won?
@@ -34,7 +44,10 @@ class Game
   end
 
   def over?
-    self.draw? || self.won?
+    if self.draw? || self.won?
+      self.board.display
+      true
+    end
   end
 
   def winner
@@ -42,7 +55,7 @@ class Game
   end
 
   def turn
-    self.board.display
+    # self.board.display
     puts "It's your turn, player #{self.current_player.token}. Select 1-9."
     input = self.current_player.move(self.board)
     if self.board.valid_move?(input)
@@ -73,19 +86,38 @@ class Game
       puts "\nJust want to watch the ai duke it out. Fair enough.\n\n"
       player_1 = Players::Computer.new("X")
       player_2 = Players::Computer.new("O")
-    elsif player_count == 1
-      puts "\nYou want to take on the AI? Humans against machines doesn't seem fair. You can go first...\n\n"
+      puts "Let's begin!"
+      game = Game.new(player_1, player_2, Board.new)
+      game.cheating_computer = false
+      game.play
+    elsif player_count == "1"
+      puts "\nYou sure you want to take on the AI? Your funeral! No on ever said anything about us AI having to play fair!\n\n"
       player_1 = Players::Human.new("X")
       player_2 = Players::Computer.new("O")
+      puts "Let's begin!"
+      game = Game.new(player_1, player_2, Board.new)
+      game.cheating_computer = true
+      game.play
     else
-      puts "Okay then. There will be two of you. Player 1, what do you want your token top be? (X, O, :). etc. )\n\n"
-      starter = gets.chomp
+      puts "Okay then. There will be two of you. Player 1, what do you want your token to be? (X, O, :). etc. )\n\n"
+      first = gets.chomp
       puts "And the token for player 2?"
       second = gets.chomp
-      player_1 = Players::Human.new(starter)
+      player_1 = Players::Human.new(first)
       player_2 = Players::Human.new(second)
+      puts "Let's begin!"
+      game = Game.new(player_1, player_2, Board.new)
+      game.cheating_computer = false
+      game.play
     end
 
+
+    # game = Game.new(player_1, player_2, Board.new)
+
+    # game.play
+    puts "Would you like to play again? [Y/n]"
+    answer = gets.chomp
+    answer == "Y" ? game.start : "exit"
   end
 
 end
--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,10 +1,15 @@
+
+# The computer's strategy is to cheat. See #current_player in Game
+# Computer will take two turns for every one turn by a Human
+# All without breaking any tests!
+
 module Players
   class Computer < Player
 
     def move(board)
-      puts "Where would you like to move (1-9)?"
       possible = board.cells.map.with_index { |box, index| (index.to_i + 1).to_s if box == " "}
       possible[Random.new.rand(possible.length)]
+
     end
 
   end
